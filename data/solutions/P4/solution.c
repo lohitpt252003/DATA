7c317e09fa0bda4c550181fb048d9c352cff0c4e
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-09-16 17:49:52 +0530

    updated the soln of 4th case

--- data/solutions/P4/solution.c
+++ data/solutions/P4/solution.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d\n", a / b);
+    long long a, b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld\n", a / b);
     return 0;
 }
