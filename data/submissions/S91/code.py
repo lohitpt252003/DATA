e445cd85903104323204575e38151c713e5774b7
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-10-09 11:07:01 +0530

    [AUTO] Add S91 code

---
+++ data/submissions/S91/code.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+try:
+    a, b = map(int, input().split())
+        print(gcd(a, b))
+except (ValueError, IndexError):
+    pass
