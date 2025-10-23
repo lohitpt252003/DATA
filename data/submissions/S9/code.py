a202ba9165e02e3d03f85c99e91b7669a26893f8
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-10-23 19:18:19 +0530

    [AUTO] Add S9 code

--- data/submissions/S9/code.py
+++ data/submissions/S9/code.py
@@ -1,1 +1,18 @@
-print('Hello World')
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+try:
+    n = int(input())
+    if is_prime(n):
+        print("Prime")
+    else:
+        print("Not Prime")
+except (ValueError, EOFError):
+    pass
