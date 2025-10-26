a305e3b60623c5fd5cf7e25716761b972944b526
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-10-26 16:28:57 +0530

    Create submission 68fdfec4978bc3c7623d2fff code file

---
+++ data/submissions/68fdfec4978bc3c7623d2fff/code.py
@@ -0,0 +1,15 @@
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input())
+if is_prime(n):
+    print("Prime")
+else:
+    print("Not Prime")
